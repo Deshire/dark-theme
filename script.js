63bfa289a53a5c2a693000ec15ef19395d3eca84
Deshire Nuñez <rivas.deshire@gmail.com>
2023-09-05 12:19:03 -0500

    dark mod commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const body = document.querySelector('body')
+
+const toogle = document.getElementById('#toggle')
+
+toggle.onclick = function(){
+    toggle.classList.toggle("active");
+    body.classList.toggle("active");
+    
+}
+
